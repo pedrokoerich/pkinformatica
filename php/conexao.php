7ed993879a4960bdee12b7dd4513f554909e8833
Pedro H. Koerich <phenriquekk@hotmail.com>
2022-03-25 00:57:33 -0300

    PK Informática - Catálogo TI

--- php/conexao.php
+++ php/conexao.php
@@ -1,5 +1,6 @@
 <?php
     //Abrir conexão com o Banco de Dados
     //$conn = mysqli_connect("servidor","usuario","senha","banco") or die("Falha: " . mysqli_connect_error());
-    $conn = mysqli_connect("localhost","root","","pk_informatica") or die("Falha: " . mysqli_connect_error());
+    $DATABASE_URL = "postgres://dmfjfotinrnwnv:3886184625c408901797092b0d9a2b039221d86ff9da3ed22b1c88f79ed5a65b@ec2-34-224-226-38.compute-1.amazonaws.com:5432/d4gj04lvtsrncp";
+    $conn = mysqli_connect($DATABASE_URL) or die("Falha: " . mysqli_connect_error());
 ?>
