1cb7d15f5d0f66adf3153eaaa18c963637ea5e1b
Pedro H. Koerich <phenriquekk@hotmail.com>
2022-03-25 01:33:35 -0300

    PK Informática - Catálogo TI

--- php/conexao.php
+++ php/conexao.php
@@ -1,7 +1,7 @@
 <?php
     //Abrir conexão com o Banco de Dados
     //$conn = mysqli_connect("servidor","usuario","senha","banco") or die("Falha: " . mysqli_connect_error());
-    $conn = mysqli_connect("ec2-34-224-226-38.compute-1.amazonaws.com","dmfjfotinrnwnv","3886184625c408901797092b0d9a2b039221d86ff9da3ed22b1c88f79ed5a65b","postgres") or die("Falha: " . mysqli_connect_error());
+    $conn = mysqli_connect("ec2-3-225-213-67.compute-1.amazonaws.com","gcfpkwwvvgjawe","c64e3bcbf41eb291b19bdb71b63cbb813c5eff248fb0db8b498bcef0e42f455e","postgres") or die("Falha: " . mysqli_connect_error());
     /* $DATABASE_URL = "postgres://dmfjfotinrnwnv:3886184625c408901797092b0d9a2b039221d86ff9da3ed22b1c88f79ed5a65b@ec2-34-224-226-38.compute-1.amazonaws.com:5432/d4gj04lvtsrncp"; */
    /*  $conn = mysqli_connect($DATABASE_URL) or die("Falha: " . mysqli_connect_error()); */
 ?>
