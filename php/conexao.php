dfa70980f67a1e220f6781f6fb5684ebedae55e3
Pedro H. Koerich <phenriquekk@hotmail.com>
2022-04-02 14:46:23 -0300

    PK Informática

--- php/conexao.php
+++ php/conexao.php
@@ -1,11 +1,11 @@
 <?php
     //Abrir conexão com o Banco de Dados
-    $url = parse_url(getenv("mysql://b36585fb7b6d35:812911aa@us-cdbr-east-05.cleardb.net/heroku_506cdc25d3cbb9f?reconnect=true"));
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
     $server = $url["host"];
     $username = $url["user"];
     $password = $url["pass"];
     $db = substr($url["path"], 1);
-    $conn = new mysqli($server, $username, $password, $db) or die("Falha: " . mysqli_connect_error());
+    $conn = new mysql($server, $username, $password, $db) or die("Falha: " . mysql_connect_error());
 
     //mysql://b36585fb7b6d35:812911aa@us-cdbr-east-05.cleardb.net/heroku_506cdc25d3cbb9f?reconnect=true
 
