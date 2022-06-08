1b3e8ed8ba99e2a6bd8a41d7ad322cc0ec146d72
Pedro H. Koerich <phenriquekk@hotmail.com>
2022-06-08 01:17:33 -0300

    PK Informática - Catáloto TI

--- php/conexao.php
+++ php/conexao.php
@@ -1,12 +1,15 @@
 <?php
     //Abrir conexão com o Banco de Dados
-    $CLEARDB_DATBASE_URL = 'mysql://b36585fb7b6d35:812911aa@us-cdbr-east-05.cleardb.net/heroku_506cdc25d3cbb9f?reconnect=true';
 
-    $url = parse_url(getenv($CLEARDB_DATBASE_URL));
-    $server = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $db = substr($url["path"], 1);
-    $conn = new mysql($server, $username, $password, $db) or die("Falha: " . mysql_connect_error());
+    $cleardb_url = parse_url ( getenv ( "CLEARDB_DATABASE_URL" ) ) ;  
+    $cleardb_server = $cleardb_url [ "eu-cdbr-west-02.cleardb.net" ] ;  
+    $cleardb_username = $cleardb_url [ "b508dff0e75fef" ] ;  
+    $cleardb_password = $cleardb_url [ "83ea8554" ] ;  
+    $cleardb_db = substr ( $cleardb_url [ "mysql://b508dff0e75fef:83ea8554@eu-cdbr-west-02.cleardb.net/heroku_0aa435053d23286?reconnect=true" ] , 1 ) ;  
+    $active_group = 'padrão' ;  
+    $query_builder = TRUE ; 
+     
+    // Conecta ao banco de dados
+    $conn = mysqli_connect ( $cleardb_server , $cleardb_username , $cleardb_password , $cleardb_db ) ;
 
 ?>
