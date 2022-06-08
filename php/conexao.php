c257a5fc140f397b7d85e27c744ab53fcc6f9ae2
Pedro H. Koerich <phenriquekk@hotmail.com>
2022-06-08 01:36:50 -0300

    PK Informática - Catálogo TI

--- php/conexao.php
+++ php/conexao.php
@@ -1,7 +1,7 @@
 <?php
     //Abrir conexão com o Banco de Dados
 
-    $cleardb_url = parse_url ( getenv ( "CLEARDB_DATABASE_URL" ) ) ;  
+/*     $cleardb_url = parse_url ( getenv ( "CLEARDB_DATABASE_URL" ) ) ;  
     $cleardb_server = $cleardb_url [ "eu-cdbr-west-02.cleardb.net" ] ;  
     $cleardb_username = $cleardb_url [ "b508dff0e75fef" ] ;  
     $cleardb_password = $cleardb_url [ "83ea8554" ] ;  
@@ -9,6 +9,17 @@
     $active_group = 'padrão' ;  
     $query_builder = TRUE ; 
      
+    // Conecta ao banco de dados
+    $conn = mysqli_connect ( $cleardb_server , $cleardb_username , $cleardb_password , $cleardb_db ) ; */
+
+    //Obter informações de conexão do Heroku ClearDB
+    $cleardb_url = parse_url ( getenv ( "CLEARDB_DATABASE_URL" ) ) ;  
+    $cleardb_server = $cleardb_url [ "host" ] ;  
+    $cleardb_username = $cleardb_url [ "user" ] ;  
+    $cleardb_password = $cleardb_url [ "pass" ] ;  
+    $cleardb_db = substr ( $cleardb_url [ "path" ] , 1 ) ;  
+    $active_group = 'default' ;  
+    $query_builder = TRUE ;  
     // Conecta ao banco de dados
     $conn = mysqli_connect ( $cleardb_server , $cleardb_username , $cleardb_password , $cleardb_db ) ;
 
