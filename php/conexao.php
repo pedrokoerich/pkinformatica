b2c90a4c3812fdc764edf034839627db34cf51b7
Pedro Koerich <pedro.koerich@totvs.com.br>
2023-01-30 23:05:29 -0300

    Ajuste de Conexão

--- php/conexao.php
+++ php/conexao.php
@@ -1,5 +1,5 @@
 <?php
-    //Abrir conexão com o Banco de Dados
+/*     //Abrir conexão com o Banco de Dados
 
     //Obter informações de conexão do Heroku ClearDB
     $cleardb_url = parse_url ( getenv ( "CLEARDB_DATABASE_URL" ) ) ;  
@@ -10,5 +10,12 @@
     $active_group = 'default' ;  
     $query_builder = TRUE ;  
     // Conecta ao banco de dados
-    $conn = mysqli_connect ( $cleardb_server , $cleardb_username , $cleardb_password , $cleardb_db) ;
+    $conn = mysqli_connect ( $cleardb_server , $cleardb_username , $cleardb_password , $cleardb_db) ; */
+
+    //Abrir conexão com o Banco de Dados
+    //$conn = mysqli_connect("servidor","usuario","senha","banco") or die("Falha: " . mysqli_connect_error());
+    $conn = mysqli_connect("localhost","root","","pk_informatica") or die("Falha: " . mysqli_connect_error());
+
+    $conn = mysqli_connect("mysql://b508dff0e75fef:83ea8554@eu-cdbr-west-02.cleardb.net/heroku_0aa435053d23286?reconnect=true") or die("Falha: " . mysqli_connect_error());
+    echo($DATABASE_URL);
 ?>
